aa43bc3987c843b1056dca9d289dce3b002a9dc5
FanHuaCloud <1157173310@qq.com>
2018-02-03 14:14:08 +0800

    增加文件 v0.1完成

--- zsh.sh
+++ zsh.sh
@@ -5,19 +5,40 @@
 
 #关闭selinux
 function Coloseselinux() {
-  echo "${CSUCCESS}[INFO]${CMSG}关闭SELINUX${CEND}"
+  echo "${CSUCCESS}[INFO]${CBLUE}关闭SELINUX${CEND}"
   [ -s /etc/selinux/config ] && sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config
 setenforce 0 >/dev/null 2>&1
 }
 
 #设置时区并同步时间
 function Settimezone() {
-  echo "${CSUCCESS}[INFO]${CMSG}设置服务器时区${CEND}"
+  echo "${CSUCCESS}[INFO] ${CBLUE}设置服务器时区${CEND}"
   rm -rf /etc/localtime
   ln -s /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
-  echo "${CSUCCESS}[INFO]${CMSG}同步服务器时间${CEND}"
+  echo "${CSUCCESS}[INFO] ${CBLUE}同步服务器时间${CEND}"
   ntpdate cn.ntp.org.cn
+}
+
+function InstallZSH() {
+  echo "${CSUCCESS}[INFO] ${CBLUE}安装ZSH${CEND}"
+  yum -y install git zsh
+  chsh -s /bin/zsh
+}
+
+function InstallOMS() {
+  echo "${CSUCCESS}[INFO] ${CBLUE}安装oh-my-zsh${CEND}"
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+}
+
+function InstallSF() {
+  echo '' > /etc/motd
+  echo "${CSUCCESS}[INFO] ${CBLUE}安装screenFetch${CEND}"
+  cp -r ./include/screenfetch-dev /usr/bin/
+  cp -r ./include/logo.sh /etc/profile.d/
 }
 
 Coloseselinux
 Settimezone
+InstallZSH
+InstallOMS
+InstallSF
